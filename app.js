9c1c0534374f96d60e45dc8fa5273177b84642f3
madeleinewoodbury <randine25@gmail.com>
2020-04-12 10:08:47 -0500

    configure email inn app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// Load env variables
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+
+// Bring in sendgrid mail and set your API Key
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Configure message
+const msg = {
+  to: 'test@example.com',
+  from: process.env.FROM_EMAIL,
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
+sgMail.send(msg);
+console.log(`Email sent to ${msg.to}`);
